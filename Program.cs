82cb3ef17ba73fde4969fdf9905f356e46ca1c2c
Jhonathan Tanada <jhonathantanada245@gmail.com>
2021-10-07 20:53:09 -0300

    Novo menu adicionado e ajustes em variaveis

--- Program.cs
+++ Program.cs
@@ -9,38 +9,79 @@ namespace StopWatch
         {
             Menu();
         }
-        
         public static void Menu()
-        {   
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("S = Segundos => 10s");
-            Console.WriteLine("M = Minutos = > 10m");
-            Console.WriteLine("Para sair aperte 0");
-            Console.WriteLine("===================");
+        {
+            Console.Clear();
+            WriteMenu();
 
-            var dados = Console.ReadLine().ToUpper();
-            var tipo = Convert.ToChar(dados.Substring(dados.Length -1, 1));
-            var time = Convert.ToInt16(dados.Substring(0, dados.Length -1));
-            var multiplicador = 1;
+            Console.SetCursorPosition(3, 1);
+            Console.WriteLine("StopWatch");
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("===================");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("S - Segundos => 10s");
+            Console.SetCursorPosition(3, 5);
+            Console.WriteLine("M - Minutos => 1m");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("===================");
+            Console.SetCursorPosition(3, 8);
+            Console.Write("Cronometrando: ");
             
-            if(tipo == 'm')
-                multiplicador = 60;
-        
+            var data = Console.ReadLine();
+            var type = char.Parse(data.Substring(data.Length - 1, 1));
+            var time = int.Parse(data.Substring(0 , data.Length -1));
+            var multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+
             if(time == 0)
                 Environment.Exit(0);
 
-            PreStarter(time * multiplicador);
-
+             PreStart(time * multiplier)  ;     
 
         }
-        public static void PreStarter(int time)
+        public static void PreStart(int time)
         {
             var currentTime = 0;
-            Console.Clear();
-            currentTime++;
-            Console.WriteLine(currentTime);
-            Thread.Sleep(1000);
-        }
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            while(currentTime != time)
+            {
+                currentTime++;
+                Console.SetCursorPosition(3, 9);
+                Console.WriteLine($"Tempo Corrido: {currentTime}");
+                Console.SetCursorPosition(3, 15);
+                Thread.Sleep(1000);
+            }
 
+        }
+        public static void WriteMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Write('|');
+            for(int i = 0; i <= 30; i++)
+                Console.Write('=');
+
+            Console.WriteLine('|');
+            for(int x = 0; x <= 10; x++)
+            {
+                Console.Write('|');
+                for(int z =  0; z <= 30; z++)
+                    Console.Write(' ');
+
+                Console.WriteLine('|'); 
+            }
+            Console.Write('|');
+            for(int a = 0; a <= 30; a++)
+                Console.Write('=');
+
+            Console.WriteLine('|');         
+
+        }
     }
 }
